5806def00f88332458b135e67c174034dfb5d644
surajgm-coder <suraj.gm@nkenters.com>
2025-12-27 15:29:41 +0530

    Update slider.js

--- wordpress-theme/nk-enterprises/js/slider.js
+++ wordpress-theme/nk-enterprises/js/slider.js
@@ -1,8 +1,58 @@
-let slides = document.querySelectorAll('.slide');
-let index = 0;
+document.addEventListener("DOMContentLoaded", function () {
 
-setInterval(() => {
-  slides[index].classList.remove('active');
-  index = (index + 1) % slides.length;
-  slides[index].classList.add('active');
-}, 5000);
+  const slides = document.querySelectorAll(".slide");
+  const dots = document.querySelectorAll(".dot");
+  const next = document.querySelector(".next");
+  const prev = document.querySelector(".prev");
+
+  let index = 0;
+  let timer;
+
+  function showSlide(i) {
+    slides.forEach(slide => slide.classList.remove("active"));
+    dots.forEach(dot => dot.classList.remove("active"));
+
+    slides[i].classList.add("active");
+    dots[i].classList.add("active");
+
+    index = i;
+  }
+
+  function nextSlide() {
+    index = (index + 1) % slides.length;
+    showSlide(index);
+  }
+
+  function prevSlide() {
+    index = (index - 1 + slides.length) % slides.length;
+    showSlide(index);
+  }
+
+  function startAutoSlide() {
+    timer = setInterval(nextSlide, 5000);
+  }
+
+  function resetAutoSlide() {
+    clearInterval(timer);
+    startAutoSlide();
+  }
+
+  next.addEventListener("click", () => {
+    nextSlide();
+    resetAutoSlide();
+  });
+
+  prev.addEventListener("click", () => {
+    prevSlide();
+    resetAutoSlide();
+  });
+
+  dots.forEach(dot => {
+    dot.addEventListener("click", () => {
+      showSlide(parseInt(dot.dataset.slide));
+      resetAutoSlide();
+    });
+  });
+
+  startAutoSlide();
+});
