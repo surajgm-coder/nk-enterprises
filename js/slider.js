d0555c289f71532705423e5ec0f1aad61fcf8846
surajgm-coder <suraj.gm@nkenters.com>
2025-12-31 15:16:54 +0530

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,19 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-  /* HEADER SCROLL EFFECT */
-  const header = document.querySelector(".site-header-wrap");
-
-  if (header) {
-    window.addEventListener("scroll", function () {
-      if (window.scrollY > 40) {
-        header.classList.add("scrolled");
-      } else {
-        header.classList.remove("scrolled");
-      }
-    });
-  }
-
-});
 /* ==========================
      MOBILE NAV TOGGLE (FIXED)
   ========================== */
